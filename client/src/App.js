489996fc4dc4fa07bb286f6cd207a5cf9fce2912
akshat10343 <akshatnext10343@gmail.com>
2025-04-25 17:23:02 -0700

    changed
    app.js, index.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
+// src/App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import HomePage from "./HomePage";
 import SolarSavePage from "./SolarSavePage";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
+// no need to import index.css here because it's already imported in index.js
 
 function App() {
   return (
     <Router>
-      <div style={{ textAlign: "center", padding: "2rem" }}>
-        <nav style={{ marginBottom: "2rem" }}>
-          <Link to="/" style={{ marginRight: "20px", fontSize: "20px" }}>Home</Link>
-          <Link to="/solar" style={{ fontSize: "20px" }}>SolarSave</Link>
+      <div className="container">
+        <nav className="nav">
+          <Link to="/" className="nav-link">Home</Link>
+          <Link to="/solar" className="nav-link">SolarSave</Link>
         </nav>
 
         <Routes>
